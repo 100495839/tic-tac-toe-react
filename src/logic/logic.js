03bf5b6c1dcf9b293c693eb0e8ef40ca9e07d842
100495839 <100495839@alumnos.uc3m.es>
2025-07-14 12:38:39 +0200

    Add check double win and optimized diagonal win check

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -6,9 +6,9 @@ import {
 
 const checkColumnWin = ({ board, player, cellID }) => {
 	const cellColumn = cellID % BOARD_DIMENSION;
-	// Check if all cells of that column have "player"
 	let winCells = [];
 
+	// Check if all cells of that column have "player"
 	const isColumnWin = board.every((cell, index) => {
 		if (index % BOARD_DIMENSION === cellColumn) {
 			winCells.push(index);
@@ -22,9 +22,9 @@ const checkColumnWin = ({ board, player, cellID }) => {
 
 const checkRowWin = ({ board, player, cellID }) => {
 	const cellRow = Math.floor(cellID / BOARD_DIMENSION);
-	// Check if all cells of that row have "player"
 	let winCells = [];
 
+	// Check if all cells of that row have "player"
 	const isRowWin = board.every((cell, index) => {
 		if (Math.floor(index / BOARD_DIMENSION) === cellRow) {
 			winCells.push(index);
@@ -37,28 +37,32 @@ const checkRowWin = ({ board, player, cellID }) => {
 };
 
 const checkDiagonalWin = ({ board, player, cellID }) => {
+	let diagonalsToCheck = [];
 	let winCells = [];
 	let isDiagonalWin = false;
 
-	DIAGONALS.forEach((diagonal) => {
-		if (!isDiagonalWin) {
-			winCells = [];
-			isDiagonalWin = diagonal.every((cellID) => {
-				winCells.push(cellID);
-				return board[cellID] === player;
-			});
+	// We have to be wary of cells may be in both diagonals
+	// If cellID is one of those cells, we cannot infer in which diagonal they could have won
+	DIAGONALS.forEach((diagonal, index) => {
+		if (diagonal.includes(cellID)) {
+			diagonalsToCheck.push(index);
+		}
+	});
+
+	diagonalsToCheck.forEach((diagonalID) => {
+		const isDiagonalFilled = DIAGONALS[diagonalID].every((cellID) => {
+			return board[cellID] === player;
+		});
+		if (isDiagonalFilled) {
+			winCells = winCells.concat(DIAGONALS[diagonalID]);
+			isDiagonalWin = true;
 		}
 	});
 
 	return { isDiagonalWin, winCells };
 };
 
-const checkWin = ({ board, player, cellID }) => {
-	const numMoves = board.filter((cell) => cell === player).length;
-	if (numMoves < 3) {
-		return { isWin: false };
-	}
-
+const checkSingleWin = ({ board, player, cellID }) => {
 	const { isColumnWin, winCells: winCellsColumn } = checkColumnWin({
 		board,
 		player,
@@ -89,12 +93,71 @@ const checkWin = ({ board, player, cellID }) => {
 	return { isWin: false };
 };
 
+const checkDoubleWin = ({ board, player, cellID }) => {
+	let numWinsLeft = 2;
+	let winCells = [];
+
+	const { isColumnWin, winCells: winCellsColumn } = checkColumnWin({
+		board,
+		player,
+		cellID,
+	});
+	if (isColumnWin) {
+		winCells = winCells.concat(winCellsColumn);
+		numWinsLeft--;
+	}
+
+	const { isRowWin, winCells: winCellsRow } = checkRowWin({
+		board,
+		player,
+		cellID,
+	});
+	if (isRowWin) {
+		winCells = winCells.concat(winCellsRow);
+		numWinsLeft--;
+	}
+
+	const { isDiagonalWin, winCells: winCellsDiagonal } = checkDiagonalWin({
+		board,
+		player,
+		cellID,
+	});
+	if (isDiagonalWin && numWinsLeft !== 0) {
+		winCells = winCells.concat(winCellsDiagonal);
+		numWinsLeft--;
+	}
+
+	if (numWinsLeft === 2) {
+		return { isWin: false };
+	}
+
+	return { isWin: true, winCells };
+};
+
+const checkWin = ({ board, player, cellID }) => {
+	const numMoves = board.filter((cell) => cell === player).length;
+	if (numMoves < 3) {
+		return { isWin: false };
+	} else {
+		let isWin, winCells;
+
+		if (numMoves === 5) {
+			({ isWin, winCells } = checkDoubleWin({ board, player, cellID }));
+		} else {
+			({ isWin, winCells } = checkSingleWin({ board, player, cellID }));
+		}
+
+		return { isWin, winCells };
+	}
+};
+
 export const checkGameResult = ({ board, player, cellID }) => {
 	const { isWin, winCells } = checkWin({ board, player, cellID });
 	if (isWin) {
 		return { gameResult: GAME_RESULT.WIN, winCells };
 	}
 
+	// If there is no win but there are no moves left either
 	const isBoardFull = !board.includes(null);
 	if (isBoardFull) {
 		return { gameResult: GAME_RESULT.TIE };
